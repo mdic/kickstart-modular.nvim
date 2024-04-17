ba8dbe5d0a53688e529464d823e7c1ae3f274fd3
mdic <deek85@gmail.com>
2024-04-17 22:24:43 +0200

    testing venv-selector; added venv desc in whichkey; disabled betterTerm and code-runner;

---
+++ lua/custom/plugins/venv-selector.lua
@@ -0,0 +1,20 @@
+return {
+  'linux-cultist/venv-selector.nvim',
+  dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+  opts = {
+    -- Your options go here
+    -- name = 'venv_*',
+    -- auto_refresh = false,
+    search_venv_managers = false,
+    path = '~/venvs/',
+    -- set parents to 0 so that it only looks in the ~/venvs/ folder!
+    parents = 0,
+  },
+  event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  keys = {
+    -- Keymap to open VenvSelector to pick a venv.
+    { '<leader>vs', '<cmd>VenvSelect<cr>', { desc = '[S]elect venv from list' } },
+    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+    { '<leader>vc', '<cmd>VenvSelectCached<cr>', { desc = 'Select [c]ached venv' } },
+  },
+}
